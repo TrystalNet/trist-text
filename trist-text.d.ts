749b786f97d474498a7dd88f77452eac733d03c3
Terry O'Neill <terry.oneill@trystal.net>
2016-07-13 14:15:11 -0600

    fix the d.ts file

--- trist-text.d.ts
+++ trist-text.d.ts
@@ -1,4 +1,4 @@
 declare module "@trystal/trist-text" {
-  import {ChainIM} from '@trystal/interfaces'
-  export function textToChain(text: string, fnNextId: (index: number) => string): ChainIM;
+  import {IMM} from '@trystal/interfaces'
+  export function textToChain(text: string, fnNextId: (index: number) => string): IMM.Chain;
 }
